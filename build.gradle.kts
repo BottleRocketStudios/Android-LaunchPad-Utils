28b3517b000bcd565dec7f7cbdf0462848be8906
Jacob Taggart <jacob.taggart@bottlerocketstudios.com>
2023-02-02 09:59:15 -0500

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath(Config.BuildScriptPlugins.ANDROID_GRADLE)
+        classpath(Config.BuildScriptPlugins.KOTLIN_GRADLE)
+    }
+}
+
+plugins {
+    id(Config.ApplyPlugins.KT_LINT) version Config.KTLINT_GRADLE_VERSION
+    id(Config.ApplyPlugins.MAVEN_PUBLISH)
+    id(Config.ApplyPlugins.DETEKT) version Config.DETEKT_VERSION
+}
+
+detekt {
+    config = files("$rootDir/config/detekt/detekt.yml") // point to your custom config defining rules to run, overwriting default behavior
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+    }
+}
---
+++ launchpad-utils/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id(Config.ApplyPlugins.ANDROID_LIBRARY)
+    kotlin(Config.ApplyPlugins.Kotlin.ANDROID)
+    id(Config.ApplyPlugins.MAVEN_PUBLISH)
+}
+
+android {
+    compileSdk = Config.AndroidSdkVersions.COMPILE_SDK
+    buildToolsVersion = Config.AndroidSdkVersions.BUILD_TOOLS
+    namespace = "com.bottlerocketstudios.launchpad.utils"
+
+    defaultConfig {
+        minSdk = Config.AndroidSdkVersions.MIN_SDK
+        targetSdk = Config.AndroidSdkVersions.TARGET_SDK
+        aarMetadata {
+            minCompileSdk = Config.AndroidSdkVersions.MIN_COMPILE_SDK
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    kotlinDependencies()
+    coroutineCoreDependency()
+
+}
