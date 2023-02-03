ed3567008a0adbe706a0fa402f24e1d7dea8e00f
Jacob Taggart <jacob.taggart@bottlerocketstudios.com>
2023-02-03 12:28:35 -0500

    ASAA-49 - add checks for debug/internal build before logging anything

--- launchpad-utils/src/main/java/com/bottlerocketstudios/launchpad/utils/logger/TimberLogger.kt
+++ launchpad-utils/src/main/java/com/bottlerocketstudios/launchpad/utils/logger/TimberLogger.kt
@@ -1,22 +1,56 @@
 package com.bottlerocketstudios.launchpad.utils.logger
 
+import com.bottlerocketstudios.launchpad.utils.BuildConfigProvider
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 import timber.log.Timber
 
-class TimberLogger : LoggingManager {
-    override fun v(tag: String?, message: String?, vararg timberArgs: Any?, t: Throwable?) =
-        tag?.let { Timber.tag(it).v(t, message, timberArgs) } ?: Timber.v(t, message, timberArgs)
+class TimberLogger : LoggingManager, KoinComponent {
 
-    override fun d(tag: String?, message: String?, vararg timberArgs: Any?, t: Throwable?) =
-        tag?.let { Timber.tag(it).d(t, message, timberArgs) } ?: Timber.d(t, message, timberArgs)
+    private val buildConfigProvider: BuildConfigProvider by inject()
+    override fun v(tag: String?, message: String?, vararg timberArgs: Any?, t: Throwable?) {
+        if (buildConfigProvider.isDebugOrInternalBuild) {
+            tag?.let {
+                Timber.tag(it).v(t, message, timberArgs)
+            } ?: Timber.v(t, message, timberArgs)
+        }
+    }
 
-    override fun i(tag: String?, message: String?, vararg timberArgs: Any?, t: Throwable?) =
-        tag?.let { Timber.tag(it).i(t, message, timberArgs) } ?: Timber.i(t, message, timberArgs)
+    override fun d(tag: String?, message: String?, vararg timberArgs: Any?, t: Throwable?) {
+        if (buildConfigProvider.isDebugOrInternalBuild) {
+            tag?.let {
+                Timber.tag(it).d(t, message, timberArgs)
+            } ?: Timber.d(t, message, timberArgs)
+        }
+    }
 
-    override fun w(tag: String?, message: String?, vararg timberArgs: Any?, t: Throwable?) =
-        tag?.let { Timber.tag(it).w(t, message, timberArgs) } ?: Timber.w(t, message, timberArgs)
+    override fun i(tag: String?, message: String?, vararg timberArgs: Any?, t: Throwable?) {
+        if (buildConfigProvider.isDebugOrInternalBuild) {
+            tag?.let {
+                Timber.tag(it).i(t, message, timberArgs)
+            } ?: Timber.i(t, message, timberArgs)
+        }
+    }
 
-    override fun e(tag: String?, message: String?, vararg timberArgs: Any?, t: Throwable?) =
-        tag?.let { Timber.tag(it).e(t, message, timberArgs) } ?: Timber.e(t, message, timberArgs)
-    override fun wtf(tag: String?, message: String?, vararg timberArgs: Any?, t: Throwable?) =
-        tag?.let { Timber.tag(it).wtf(t, message, timberArgs) } ?: Timber.wtf(t, message, timberArgs)
+    override fun w(tag: String?, message: String?, vararg timberArgs: Any?, t: Throwable?) {
+        if (buildConfigProvider.isDebugOrInternalBuild) {
+            tag?.let {
+                Timber.tag(it).w(t, message, timberArgs)
+            } ?: Timber.w(t, message, timberArgs)
+        }
+    }
+
+    override fun e(tag: String?, message: String?, vararg timberArgs: Any?, t: Throwable?) {
+        if (buildConfigProvider.isDebugOrInternalBuild) {
+            tag?.let {
+                Timber.tag(it).e(t, message, timberArgs)
+            } ?: Timber.e(t, message, timberArgs)
+        }
+    }
+
+    override fun wtf(tag: String?, message: String?, vararg timberArgs: Any?, t: Throwable?) {
+        tag?.let {
+            Timber.tag(it).wtf(t, message, timberArgs)
+        } ?: Timber.wtf(t, message, timberArgs)
+    }
 }
