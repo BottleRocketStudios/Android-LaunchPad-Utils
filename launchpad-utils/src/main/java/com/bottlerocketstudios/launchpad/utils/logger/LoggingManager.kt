7a5258de0314ed02bf50b45893101a5aeee88a7c
Jacob Taggart <jacob.taggart@bottlerocketstudios.com>
2023-02-06 15:55:18 -0500

    ASAA-49 - add string helper class, default params to null in logger interface

--- launchpad-utils/src/main/java/com/bottlerocketstudios/launchpad/utils/logger/LoggingManager.kt
+++ launchpad-utils/src/main/java/com/bottlerocketstudios/launchpad/utils/logger/LoggingManager.kt
@@ -3,10 +3,10 @@ package com.bottlerocketstudios.launchpad.utils.logger
 /** Wrapper for Logging utility. Implement either the TimberLogger or Android Logger in DI. */
 interface LoggingManager {
     /** If using AndroidLogger, note [timberArgs] will be ignored. */
-    fun v(tag: String?, message: String?, vararg timberArgs: Any? = emptyArray(), t: Throwable?)
-    fun d(tag: String?, message: String?, vararg timberArgs: Any? = emptyArray(), t: Throwable?)
-    fun i(tag: String?, message: String?, vararg timberArgs: Any? = emptyArray(), t: Throwable?)
-    fun w(tag: String?, message: String?, vararg timberArgs: Any? = emptyArray(), t: Throwable?)
-    fun e(tag: String?, message: String?, vararg timberArgs: Any? = emptyArray(), t: Throwable?)
-    fun wtf(tag: String?, message: String?, vararg timberArgs: Any? = emptyArray(), t: Throwable?)
+    fun v(tag: String? = null, message: String?, vararg timberArgs: Any? = emptyArray(), t: Throwable? = null)
+    fun d(tag: String? = null, message: String?, vararg timberArgs: Any? = emptyArray(), t: Throwable? = null)
+    fun i(tag: String? = null, message: String?, vararg timberArgs: Any? = emptyArray(), t: Throwable? = null)
+    fun w(tag: String? = null, message: String?, vararg timberArgs: Any? = emptyArray(), t: Throwable? = null)
+    fun e(tag: String? = null, message: String?, vararg timberArgs: Any? = emptyArray(), t: Throwable? = null)
+    fun wtf(tag: String? = null, message: String?, vararg timberArgs: Any? = emptyArray(), t: Throwable? = null)
 }
