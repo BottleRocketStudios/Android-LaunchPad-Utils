0aa2298b5ce40e57f9c381014c29df7128873d26
Eric Emery <eric.emery@bottlerocketstudios.com>
2023-03-24 10:22:15 -0500

    ASAA-56 - Removed domin files

--- launchpad-utils/src/main/java/com/bottlerocketstudios/launchpad/utils/analytics/BaseAnalyticsUserProperty.kt
+++
@@ -1,5 +0,0 @@
-package com.bottlerocketstudios.launchpad.utils.analytics
-
-interface BaseAnalyticsUserProperty {
-    fun createUserProperty(): Any
-}
--- launchpad-utils/src/main/java/com/bottlerocketstudios/launchpad/utils/analytics/UserProperty.kt
+++
@@ -1,9 +0,0 @@
-package com.bottlerocketstudios.launchpad.utils.analytics
-
-/** Used in [AnalyticsInterface.setUserProperty]. */
-interface UserProperty : BaseAnalyticsUserProperty {
-    val propertyName: String
-    val propertyValue: String
-
-    override fun createUserProperty(): UserProperty = CustomUserProperty(propertyName, propertyValue)
-}
